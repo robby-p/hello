5dd130921aff22d5c8c6de41cc1f6cd8cb099f1f
Robby Polana <rpolana@squarespace.com>
2020-05-25 17:00:58 -0400

    - Fixed requirements - removed happyfuntime stuff, just blog app now - home '/' goes to blog home

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import include, path
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 
 from . import views
 
+
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
+    path("", views.PostList.as_view(), name="home"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("admin/", admin.site.urls),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html")),
+    path("post/<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
 ]
